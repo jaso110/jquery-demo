091b49471980f0a7bf358fda0e45f0fd61c2f3b1
Jan Sørensen <jaso110@gmail.com>
2022-08-11 22:09:26 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$("h1").css({"color": "red"});
+// jQuery, extract all Links From a URL
+
+$(document).ready(function() {
+    $('a').each(function() {
+      console.log($(this).attr('href'));
+      $(".link-demo").append("<a href=" + $(this).attr('href') + ">" + $(this).attr('href') + "</a><br><br>");
+    });
+});
